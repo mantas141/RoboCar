7072bf95f9986b2818f0f8fb0575b0a94dabb305
Symaro <37690011+Symaro@users.noreply.github.com>
2018-04-22 17:06:30 +0200

    Add files via upload
    Added ir_test.py and line_following.py

---
+++ ir_test.py
@@ -0,0 +1,22 @@
+# External module imports
+import RPi.GPIO as GPIO
+import time
+
+# Pin Definitons:
+IRsensor1 = 12 # Broadcom pin - Up for change
+IRsensor2 = 16 # Broadcom pin - Up for change
+IRsensor3 = 20 # Broadcom pin - Up for change
+IRsensor4 = 21 # Broadcom pin - Up for change
+
+# Pin Setup:
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.setup(IRsensor1, GPIO.IN) # sensor set as input
+
+# PROGRAM - 0 = reflective surface, 1 = nonreflective surface
+while True:
+        A = GPIO.input(IRsensor1)
+		B = GPIO.input(IRsensor2)
+		C = GPIO.input(IRsensor3)
+		D = GPIO.input(IRsensor4)
+        print (A, B, C, D)
+        time.sleep(0.5)
---
+++ line_following.py
@@ -0,0 +1,67 @@
+import RPi.GPIO as GPIO
+import ir_test as ir
+import time
+
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+
+ain1 = 2
+ain2 = 3
+bin1 = 14
+bin2 = 15
+PWMA = 18
+PWMB = 13
+
+GPIO.setup(ain1, GPIO.OUT)
+GPIO.setup(ain2, GPIO.OUT)
+GPIO.setup(bin1, GPIO.OUT)
+GPIO.setup(bin2, GPIO.OUT)
+GPIO.setup(PWMA, GPIO.OUT)
+
+speed_a = GPIO.PWM(PWMA, 50)  # a is right side engines
+speed_b = GPIO.PWM(PWMB, 50)  # b is left side engines
+
+
+def forward():
+    GPIO.output(ain1, GPIO.HIGH)
+    GPIO.output(ain2, GPIO.LOW)
+    GPIO.output(bin1, GPIO.HIGH)
+    GPIO.output(bin2, GPIO.LOW)
+
+
+def backwards():
+    GPIO.output(ain1, GPIO.LOW)
+    GPIO.output(ain2, GPIO.HIGH)
+    GPIO.output(bin1, GPIO.LOW)
+    GPIO.output(bin2, GPIO.HIGH)
+
+
+def TurnRight():
+    GPIO.output(ain1, GPIO.HIGH)  # a side/right side going forwards
+    GPIO.output(ain2, GPIO.LOW)
+    GPIO.output(bin1, GPIO.LOW)  # b side/left side going backwards
+    GPIO.output(bin2, GPIO.HIGH)
+
+
+def TurnLeft():
+    GPIO.output(ain1, GPIO.LOW)  # a side/right side going backwards
+    GPIO.output(ain2, GPIO.HIGH)
+    GPIO.output(bin1, GPIO.HIGH)  # b side/left side going forwards
+    GPIO.output(bin2, GPIO.LOW)
+
+
+
+def main():
+    while True:
+        if ir.A == 0 and ir.B == 1 and ir.C == 1 and ir.D == 0:
+            forward()
+        elif ir.A == 0 and ir.B == 1 and ir.C == 0 and ir.D == 0:
+            TurnLeft()
+        elif ir.A == 1 and ir.B == 1 and ir.C == 0 and ir.D == 0:
+            TurnLeft()
+        elif ir.A == 0 and ir.B == 0 and ir.C == 1 and ir.D == 0:
+            TurnRight()
+        elif ir.A == 0 and ir.B == 0 and ir.C == 1 and ir.D == 1:
+            TurnRight()
+        time.sleep(0.1)
