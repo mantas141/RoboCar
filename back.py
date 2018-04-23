67324c54c44983dcf856c0a9b6e4f5fac189a701
Mantas <mantas141@gmail.com>
2018-04-23 11:03:43 +0200

    User controlled Python scripts. Speeds should need adjustment

---
+++ back.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+import time
+
+PWMA = 18
+PWMB = 13
+AIN1 = 2
+AIN2 = 3
+BIN1 = 14
+BIN2 = 15
+
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(AIN1, GPIO.OUT)
+GPIO.setup(AIN2, GPIO.OUT)
+GPIO.setup(BIN1, GPIO.OUT)
+GPIO.setup(BIN2, GPIO.OUT)
+GPIO.setup(PWMA, GPIO.OUT)
+GPIO.setup(PWMB, GPIO.OUT)
+
+GPIO.output(AIN1, GPIO.LOW)
+GPIO.output(AIN2, GPIO.HIGH)
+GPIO.output(BIN1, GPIO.LOW)
+GPIO.output(BIN2, GPIO.HIGH)
+
+#speeds need to be adjusted
+
+rightmotor = GPIO.PWM(PWMA, 50)
+rightmotor.start(0)
+
+rightmotor.ChangeDutyCycle(100)
+
+leftmotor = GPIO.PWM(PWMB, 50)
+leftmotor.start(0)
+
+leftmotor.ChangeDutyCycle(100)
