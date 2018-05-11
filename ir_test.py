7a27e20077d3b3fd725cacaf79154ef4ec62b597
Symaro <johnlaursen90@gmail.com>
2018-05-11 11:31:17 +0200

    Redid the setup and direction functions incll main()

--- ir_test.py
+++ ir_test.py
@@ -11,12 +11,15 @@ IRsensor4 = 21 # Broadcom pin - Up for change
 # Pin Setup:
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
 GPIO.setup(IRsensor1, GPIO.IN) # sensor set as input
+GPIO.setup(IRsensor2, GPIO.IN) # sensor set as input
+GPIO.setup(IRsensor3, GPIO.IN) # sensor set as input
+GPIO.setup(IRsensor4, GPIO.IN) # sensor set as input
 
 # PROGRAM - 0 = reflective surface, 1 = nonreflective surface
 while True:
-        A = GPIO.input(IRsensor4)
-		B = GPIO.input(IRsensor3)
-		C = GPIO.input(IRsensor2)
-		D = GPIO.input(IRsensor1)
-        print (A, B, C, D)
-        time.sleep(0.5)
+    A = GPIO.input(IRsensor4)
+    B = GPIO.input(IRsensor3)
+    C = GPIO.input(IRsensor2)
+    D = GPIO.input(IRsensor1)
+    print (A, B, C, D)
+    time.sleep(0.5)
