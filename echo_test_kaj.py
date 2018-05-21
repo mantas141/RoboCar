43d812afd63c17655957354209e02ecd37b2bc51
Symaro <johnlaursen90@gmail.com>
2018-05-21 11:23:18 +0200

    edited the echo_test_kaj for testing with the same pinouts and made a first version to hopefully test the sumo

--- echo_test_kaj.py
+++ echo_test_kaj.py
@@ -3,22 +3,22 @@ import datetime
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(15,GPIO.OUT)
-GPIO.setup(16,GPIO.IN)
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(17,GPIO.IN)
 
 
 def reading(sensor):
     pingtime = 0
     echotime = 0
     if sensor == 0:
-        GPIO.output(15,GPIO.LOW)
-        GPIO.output(15,GPIO.HIGH)
+        GPIO.output(27,GPIO.LOW)
+        GPIO.output(27,GPIO.HIGH)
         pingtime = time.time()
         time.sleep(0.00001)
-        GPIO.output(15,GPIO.LOW)
-        while GPIO.input(16)==0:
+        GPIO.output(27,GPIO.LOW)
+        while GPIO.input(17)==0:
             pingtime = time.time()
-        while GPIO.input(16)==1:
+        while GPIO.input(17)==1:
             echotime=time.time()
         if (echotime is not None) and (pingtime is not None):
             elapsedtime = echotime - pingtime
@@ -33,4 +33,4 @@ while True:
     range = reading(0)
     print(range)
     print()
-    time.sleep(0.25)
+    time.sleep(1)
--- sumo.py
+++ sumo.py
@@ -1,3 +1,15 @@
 import directions as d
 import echo as e
 import time
+
+
+# max distance 180 cm
+
+def main():
+    if any(t == 1 for t in d.ir_read()):
+        d.TurnLeft()
+    elif e.reading(0) > 180:
+        d.forward()
+    time.sleep(0.2)
+
+main()
