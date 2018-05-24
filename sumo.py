8b85dc0c08b55b92b1cc2d2e0146ff31369fffcd
Symaro <johnlaursen90@gmail.com>
2018-05-24 13:20:41 +0200

    updated sumo script

--- sumo.py
+++ sumo.py
@@ -2,14 +2,31 @@ import directions as d
 import echo as e
 import time
 
-
 # max distance 180 cm
-
+count = 0
 def main():
-    if any(t == 1 for t in d.ir_read()):
-        d.TurnLeft()
-    elif e.reading(0) > 180:
-        d.forward()
+    while count < 4:
+        scout()
+
+def backwards():
+    d.backwards()
+    time.sleep(1)
+    d.TurnLeft()
     time.sleep(0.2)
+    global count
+    count += 1
+
+
+def scout():
+    r = range(5, 180)
+    if any(t == 1 for t in d.ir_read()):
+        backwards()
+    elif e.reading(0) < 180 and any(t == 0 for t in d.ir_read()):
+        d.TurnLeft()
+    elif e.reading(0) in r and any(t == 0 for t in d.ir_read()):
+        d.forward()
+
+
+
 
 main()
